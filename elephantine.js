8ef55c41bcc4d7ad78a89f9dcee2df18dd8fe759
Beau Gunderson <beau@beaugunderson.com>
2017-01-31 14:16:18 -0800

    add expand; maxIterations argument

--- elephantine.js
+++ elephantine.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.clean = require('./lib/clean-curve.js');
+exports.expand = require('./lib/expand-curve.js');
 exports.generate = require('./lib/generate-curve.js');
 exports.render = require('./lib/render-curve.js');
 exports.system = require('./lib/system.js');
